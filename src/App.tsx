e2914b0bb2fd4fedcf5322789cd4c804f7c2ca5d
jingyu qi <qijingyu2009@gmail.com>
2020-03-13 00:17:15 +0800

    qijingyu
    init submit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from 'antd/es/button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        HELLO WORLD!
+      <br/>
+        <Button type="primary">enter</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
